b8f612f559abb7d786769015310cbc6cbf539360
jasonbouchez <jasonbch409@gmail.com>
2023-09-07 11:54:00 +0200

    front/panneau admin jessica

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<!-- connexion a votre site -->
+<body>
+
+    <header class="nomm">
+<div class="ma-div">
+<img  class="img1s" id="animated-gif" src="image/logojessica.gif" alt="Animation GIF">       
+      <h1 class="titlee">panneau d'administration</h1>      
+     <ul class="boutoon">
+        <li><a href="#">gestion du site</a></li>
+        <li><a href="#">gestion du compte</a></li>
+        <li><a href="#">se deconnecter</a></li>
+     </ul>
+        </header>
+        </div>
+        
+
+<script>
+  const gif = document.getElementById('animated-gif');
+
+
+  setTimeout(function() {
+    gif.src = 'image/Logojessica.png'; 
+  }, 2600); 
+</script>
+
+ <!-- formulaire de connexion -->
+ <h1 class="connexion">connexion a votre site </h1>
+    <div class='chp'>
+    <label for="">Adresse email</label>
+        <input class="placeholder" type="text">
+        </div>
+
+        <div class='chp'>
+        <label for="">Mot de passe</label>
+        <input class="placeholder" type="password" name="" id="">
+        </div>
+
+        <div class="bonbon">
+        <input type="submit" value="Se connecter">
+        </div>
+
+        <div class="bonbon">
+        <input  type="submit" value="Mot de passe oublié">
+        </div>
+
+    <!-- mots de passe oublié-->
+
+    <header class="nomm">
+<div class="ma-div">
+<img  class="img1s" id="animated-gif" src="image/logojessica.gif" alt="Animation GIF">       
+      <h1 class="titlee">panneau d'administration</h1>      
+     <ul class="boutoon">
+        <li><a href="#">gestion du site</a></li>
+        <li><a href="#">gestion du compte</a></li>
+        <li><a href="#">se deconnecter</a></li>
+     </ul>
+        </header>
+        </div>
+        
+
+<script>
+  const gif = document.getElementById('animated-gif');
+
+
+  setTimeout(function() {
+    gif.src = 'image/Logojessica.png'; 
+  }, 2600); 
+</script>
+
+        
+<!-- formulaire  d'inscription pour le mots de passe oublié-->
+    
+        <div class="form">
+        <input  type="text" size="30" placeholder="Nom" class="mdpoub">
+        <input type="text" size="30" placeholder="Prénom" class="mdpoub">
+    </div>
+
+    <div class="formm">
+        <input  type="text" size="30" placeholder="e-mail"  class="mdpoub">
+        <input type="password" size="30" placeholder="mots de passe" class="mdpoub">
+    </div>
+
+    <div class="connex">
+        <input class="necter" type="submit" value="Se connecter"> 
+        </div>
+    </div>
+    
+       
+
+<!-- page de connexion (vous pouvez gerer votre site d'ici) -->
+        
+<header class="nomm">
+<div class="ma-div">
+<img  class="img1s" id="animated-gif" src="image/logojessica.gif" alt="Animation GIF">       
+      <h1 class="titlee">panneau d'administration</h1>      
+     <ul class="boutoon">
+        <li><a href="#">gestion du site</a></li>
+        <li><a href="#">gestion du compte</a></li>
+        <li><a href="#">se deconnecter</a></li>
+     </ul>
+        </header>
+        </div>
+        
+
+<script>
+  const gif = document.getElementById('animated-gif');
+
+
+  setTimeout(function() {
+    gif.src = 'image/Logojessica.png'; 
+  }, 2600); 
+</script>   
+    <h1 class="ecriture">ICI VOUS POUVEZ GERER VOTRE SITE ET VOS COMPTES</h1>
+
+
+<!-- formulaire gestions du comptes -->
+
+<div class="inscription">
+<label for=""><a href="?"><b>Créations d'un nouvelle utilisateur</b></label></a>
+</div>
+
+<h2 class="modif"> modifier votre compte</h2>
+
+<div class='chp'>
+    <label for="">Adresse email</label>
+        <input class="placeholder" type="text">
+        </div>
+
+        <div class='chp'>
+        <label for="">Mot de passe</label>
+        <input class="placeholder" type="password" name="" id="">
+        </div>
+
+        <div class='chp'>
+        <label for=""> Confirmé Mot de passe</label>
+        <input class="placeholder" type="password" name="" id="">
+        </div>
+
+
+<!-- creation d'un nouveaux utilisateur  -->
+
+<header class="nomm">
+<div class="ma-div">
+<img  class="img1s" id="animated-gif" src="image/logojessica.gif" alt="Animation GIF">       
+      <h1 class="titlee">panneau d'administration</h1>      
+     <ul class="boutoon">
+        <li><a href="#">gestion du site</a></li>
+        <li><a href="#">gestion du compte</a></li>
+        <li><a href="#">se deconnecter</a></li>
+     </ul>
+        </header>
+        </div>
+        
+
+<script>
+  const gif = document.getElementById('animated-gif');
+
+
+  setTimeout(function() {
+    gif.src = 'image/Logojessica.png'; 
+  }, 2600); 
+</script> 
+
+<section>
+    <h1 class="modifier">Création d'un nouvel utilisateur</h1>
+   
+    <div class='chp'>
+    <label for="">Adresse email</label>
+        <input class="placeholder" type="text">
+        </div>
+
+        <div class='chp'>
+        <label for="">Mot de passe</label>
+        <input class="placeholder" type="password" name="" id="">
+        </div>
+    
+    <div class="butbut">
+    <input class="seConnecter" type="submit" value="ENVOYER">
+    </div>
+    
+    </div>
+</section>
+
+<!-- deconnexion -->
+
+<header class="nomm">
+<div class="ma-div">
+<img  class="img1s" id="animated-gif" src="image/logojessica.gif" alt="Animation GIF">       
+      <h1 class="titlee">panneau d'administration</h1>      
+     <ul class="boutoon">
+        <li><a href="#">gestion du site</a></li>
+        <li><a href="#">gestion du compte</a></li>
+        <li><a href="#">se deconnecter</a></li>
+     </ul>
+        </header>
+        </div>
+        
+
+<script>
+  const gif = document.getElementById('animated-gif');
+
+
+  setTimeout(function() {
+    gif.src = 'image/Logojessica.png'; 
+  }, 2600); 
+</script> 
+
+<section class="jaja">
+    <h1 class="titre">voulez vous vraiment vous deconnecter ?</h1>
+
+    <div class="btn2">
+    <input class="seConnecter" type="submit" value="Se deconnecter">
+     </div>
+</section>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
